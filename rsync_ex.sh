eca3f225346d08c20b72b40f7ad648b1359d616d
ravindranadhcn <ravindranadhcn@gmail.com>
2024-07-15 10:31:04 -0400

    Create rsync_ex.sh

---
+++ rsync_ex.sh
@@ -0,0 +1,8 @@
+rsync remote copy
+rsyn src/test1.txt  dest  //copy file from src folder to destination folder
+
+rsyn src dest  // It skips directory 
+
+rsyn -r src dest    //src folder copied to dest folder
+
+
